fedbd982051d30f38d388df88d220596419d3967
Mrunal <bhamaremsb@gmail.com>
2025-04-09 23:47:33 +0530

    9/4/25 codes

--- cp/Elephant.cpp
+++ cp/Elephant.cpp
@@ -10,22 +10,22 @@ int main (){
 	int b = a;
 
 	while (b > 0){
-		cout << cnt << endl;
+	
 		if(b >= 5){
-			b %= 5;
 			cnt += b/5;
+			b %= 5;
 		}
 		else if (b >= 4){
-			b %= 4;
 			cnt += 1;
+			b %= 4;
 		}
 		else if (b >= 3){
 			b %= 3;
 			cnt += 1;
 		}
 		else if (b >= 2){
-			b %= 2;
 			cnt += 1;
+			b %= 2;
 		}
 		else b = 0, cnt += 1;
 	}
